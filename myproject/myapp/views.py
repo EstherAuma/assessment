a8945dc17ba87063876268655792838a3d761cdc
Esther Auma <estheraumaego@gmail.com>
2023-09-02 21:47:51 +0300

    new commit

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,8 +3,5 @@ from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        if form_is_valid:
-
-            return HttpResponseRedirect('home')
+    
     return render(request,'index.html')
