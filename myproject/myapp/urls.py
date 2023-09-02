7c81709903ceaa081f85ba70772f2d0a9ab69bda
Esther Auma <estheraumaego@gmail.com>
2023-09-02 21:30:50 +0300

    second commit

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,4 +3,5 @@ from myapp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('registration_form/', views.registration_form, name='registration_form')
 ]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap5',
     'myapp',
+    'crispy_forms',
+    'crispy_bootstrap4',
 
 ]
 
@@ -124,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
